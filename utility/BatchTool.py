3f58fab8c4973eab196e53513d6641c23003f324
BenjaminMesic <benjamin.mesic@cern.ch>
2017-06-02 14:49:35 +0200

    Changed the way how to send jobs

--- utility/BatchTool.py
+++ utility/BatchTool.py
@@ -49,9 +49,9 @@ class BatchTool(object):
       _txt      = self.arguments['<path_batch_file_wo_ext>'] + '.txt'
       _command  = ['condor_submit', _txt]
       utility.Print('status','{0}'.format(' '.join(_command)))
-      print sp.check_output(_command)
+      sp.call(' '.join(_command), shell=True)
     
     elif self.batch == 'lxbatch':
-      _command  = ['bsub', '-q', self.queue, '-J', self.arguments['<path_batch_file_wo_ext>'] + ' < ' + _sh]
+      _command  = ['bsub', '-q', self.queue, '-J', self.arguments['<path_batch_file_wo_ext>'], '<', _sh]
       utility.Print('status','{0}'.format(' '.join(_command)))
-      print sp.check_output(_command)
+      sp.call( ' '.join(_command), shell=True)
